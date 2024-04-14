eb84e200a190e55fa547162aba1a4b432b05bada
Rei <snowy.sunny.dazing.us@gmail.com>
2024-04-15 00:33:59 +0900

    - Update

--- AMShop_Server/src/main/java/service/ServiceMessage.java
+++ AMShop_Server/src/main/java/service/ServiceMessage.java
@@ -3,14 +3,12 @@ package service;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
-import connect.MSSQLConnection;
-import jakarta.persistence.EntityManager;
 import service_interface.IMessage;
 
 public class ServiceMessage extends UnicastRemoteObject implements IMessage {
 
 	private static final long serialVersionUID = -3601247619539596175L;
-	private EntityManager entityManager;
+//	private EntityManager entityManager;
 	
 	public ServiceMessage() throws RemoteException {
 //		entityManager = MSSQLConnection.getEntityManager();
