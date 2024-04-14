1b4d902625e0331a916c8adfeccc626a17a5bc95
NguyenHuuPhuocHai <hain35284@gmail.com>
2024-04-14 22:04:51 +0700

    - Update QuanAo

--- AMShop_JPA_ORM/src/main/java/entity/QuanAo.java
+++ AMShop_JPA_ORM/src/main/java/entity/QuanAo.java
@@ -4,6 +4,10 @@ import java.io.Serializable;
 
 import javax.swing.ImageIcon;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,22 +20,51 @@ import lombok.ToString;
 @EqualsAndHashCode
 @ToString
 
+@Entity
+@Table(name = "QuanAo")
+
 public class QuanAo implements Serializable{
 	
 	private static final long serialVersionUID = 4899708714148214926L;
 	
+	
+	@Column(name = "MaQuanAo")
 	private String maQuanAo;
+	
+	@Column(name = "TenQuanAo")
 	private String tenQuanAo;
+	
+	@Column(name = "DonGiaNhap")
 	private double donGiaNhap;
+	
+	@Column(name = "DonGiaBan")
 	private double donGiaBan;
+	
+	@Column(name = "SoLuongTrongKho")
 	private int soLuongTrongKho;
+	
+	@Column(name = "NhaSanXuat")
 	private String nhaSanXuat;
+	
+	@Column(name = "DanhMuc")
 	private String danhMuc;
+	
+	@Column(name = "GioiTinh")
 	private String gioiTinh;
+	
+	@Column(name = "MauSac")
 	private String mauSac;
+	
+	@Column(name = "KichThuoc")
 	private String kichThuoc;
+	
+	@Column(name = "ChatLieu")
 	private String chatLieu;
+	
+	@Column(name = "HinhAnh")
 	private ImageIcon hinhAnh;
+	
+	@Column(name = "NgungNhap")
 	private boolean ngungNhap;
 
 }
