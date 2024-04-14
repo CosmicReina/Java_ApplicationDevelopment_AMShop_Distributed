302e0ef589e8ce2e43d4e8b8b8f956c573dfba06
NguyenHuuPhuocHai <hain35284@gmail.com>
2024-04-14 20:48:01 +0700

    - Create DonDatHang

---
+++ AMShop_JPA_ORM/src/main/java/entity/DonDatHang.java
@@ -0,0 +1,27 @@
+package entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+
+public class DonDatHang implements Serializable {
+	
+	private static final long serialVersionUID = 4899708714148214926L;
+	
+	private String maDonDatHang;
+	private NhanVien nhanVien;
+	private KhachHang khachHang;
+	private LocalDateTime thoiGianTao;
+	private boolean trangThaiThanhToan;
+}
