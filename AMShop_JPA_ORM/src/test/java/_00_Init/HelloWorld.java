065340d8f60838bb8a1f2b537e642a1b5de1ed7c
Rei <snowy.sunny.dazing.us@gmail.com>
2024-04-14 21:34:02 +0900

    - Create projects

--- AMShop_JPA_ORM/src/test/java/_00_Init/HelloWorld.java
+++ AMShop_JPA_ORM/src/test/java/_00_Init/HelloWorld.java
@@ -3,7 +3,9 @@ package _00_Init;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		HelloWorldObject obj = new HelloWorldObject("Hello World!");
+		System.out.println(obj);
+		
 	}
 	
 }
