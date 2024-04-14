979572432d8e877661a55b6865903d746be6dd9e
NguyenHuuPhuocHai <hain35284@gmail.com>
2024-04-14 20:49:23 +0700

    - Create ChiTietHoaDon

---
+++ AMShop_JPA_ORM/src/main/java/entity/ChiTietHoaDon.java
@@ -0,0 +1,25 @@
+package entity;
+
+import java.io.Serializable;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+
+public class ChiTietHoaDon implements Serializable{
+
+	private static final long serialVersionUID = 4899708714148214926L;
+	
+	private HoaDon hoaDon;
+	private QuanAo quanAo;
+	private int soLuong;
+	private double donGia;
+}
