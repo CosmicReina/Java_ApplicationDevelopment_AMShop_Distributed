289a1ef55b48d8ae40a079cf32ed2f0699ae49dd
NguyenHuuPhuocHai <hain35284@gmail.com>
2024-04-14 22:06:31 +0700

    - Update ChiTietHoaDon

--- AMShop_JPA_ORM/src/main/java/entity/ChiTietHoaDon.java
+++ AMShop_JPA_ORM/src/main/java/entity/ChiTietHoaDon.java
@@ -2,6 +2,10 @@ package entity;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,13 +18,24 @@ import lombok.ToString;
 @EqualsAndHashCode
 @ToString
 
+@Entity
+@Table(name = "ChiTietHoaDon")
+
 public class ChiTietHoaDon implements Serializable{
 
 	private static final long serialVersionUID = 4899708714148214926L;
 	
+	
+	@Column(name = "HoaDon")
 	private HoaDon hoaDon;
+	
+	@Column(name = "QuanAo")
 	private QuanAo quanAo;
+	
+	@Column(name = "SoLuong")
 	private int soLuong;
+	
+	@Column(name = "DonGia")
 	private double donGia;
 	
 }
