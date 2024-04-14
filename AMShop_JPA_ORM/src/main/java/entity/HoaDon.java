7e7a095de2d9b37f364fa885da7d38e21f76212a
NguyenHuuPhuocHai <hain35284@gmail.com>
2024-04-14 20:47:49 +0700

    - Create HoaDon

---
+++ AMShop_JPA_ORM/src/main/java/entity/HoaDon.java
@@ -0,0 +1,28 @@
+package entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+
+
+public class HoaDon implements Serializable {
+	
+	private static final long serialVersionUID = 4899708714148214926L;
+	
+	private String maHoaDon;
+	private CuaHang cuaHang;
+	private NhanVien nhanVien;
+	private KhachHang khachHang;
+	private LocalDateTime thoiGianTao;
+}
