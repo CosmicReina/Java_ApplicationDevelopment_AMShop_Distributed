ff17ee9052271369b5e51765822210ccb670119b
Rei <snowy.sunny.dazing.us@gmail.com>
2024-04-14 23:14:54 +0900

    - Update

---
+++ AMShop_Server/src/main/java/initiate/Server.java
@@ -0,0 +1,9 @@
+package initiate;
+
+public class Server {
+	
+	private static final String ADDRESS = "REI";
+	private static final String PORT = "8080";
+	private static final String URL = "rmi://" + ADDRESS + ":" + PORT + "/";
+
+}
