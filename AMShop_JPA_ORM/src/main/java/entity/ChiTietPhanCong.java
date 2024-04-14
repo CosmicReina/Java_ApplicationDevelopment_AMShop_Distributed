99bed6e850eb04cd921cae0eaf9b3757e0dc65f6
NguyenHuuPhuocHai <hain35284@gmail.com>
2024-04-14 20:30:29 +0700

    - Create ChiTIetPhanCong

---
+++ AMShop_JPA_ORM/src/main/java/entity/ChiTietPhanCong.java
@@ -0,0 +1,26 @@
+package entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+
+public class ChiTietPhanCong implements Serializable {
+	
+	private static final long serialVersionUID = 4899708714148214926L;
+	
+	private LichLamViec lichLamViec;
+	private NhanVien nhanVien;
+	private LocalDateTime thoiGianVaoCa;
+	private LocalDateTime thoiGianRaCa;
+}
