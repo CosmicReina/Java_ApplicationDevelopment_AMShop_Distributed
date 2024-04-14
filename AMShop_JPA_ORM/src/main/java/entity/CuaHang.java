b77654138f8c52c215c7ac350627ceeca220b562
NguyenHuuPhuocHai <hain35284@gmail.com>
2024-04-14 22:06:58 +0700

    - Update CuaHang

--- AMShop_JPA_ORM/src/main/java/entity/CuaHang.java
+++ AMShop_JPA_ORM/src/main/java/entity/CuaHang.java
@@ -2,6 +2,10 @@ package entity;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,13 +18,24 @@ import lombok.ToString;
 @EqualsAndHashCode
 @ToString
 
+@Entity
+@Table(name = "CuaHang")
+
 public class CuaHang implements Serializable {
 	
 	private static final long serialVersionUID = 4899708714148214926L;
 	
+	
+	@Column(name = "MaCuaHang")
 	private String maCuaHang;
+	
+	@Column(name = "TenCuaHang")
 	private String tenCuaHang;
+	
+	@Column(name = "DiaChi")
 	private String diaChi;
+	
+	@Column(name = "SoDienThoai")
 	private String soDienThoai;
 
 }
