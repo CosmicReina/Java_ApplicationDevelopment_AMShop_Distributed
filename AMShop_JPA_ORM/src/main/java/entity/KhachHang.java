d9911caf08f304a2f67e6d50f517e11425f77a21
NguyenHuuPhuocHai <hain35284@gmail.com>
2024-04-14 22:07:27 +0700

    - Update KhachHang

--- AMShop_JPA_ORM/src/main/java/entity/KhachHang.java
+++ AMShop_JPA_ORM/src/main/java/entity/KhachHang.java
@@ -2,6 +2,10 @@ package entity;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,14 +18,27 @@ import lombok.ToString;
 @EqualsAndHashCode
 @ToString
 
+@Entity
+@Table(name = "KhachHang")
+
 public class KhachHang implements Serializable{
 	
 	private static final long serialVersionUID = 4899708714148214926L;
 	
+	
+	@Column(name = "MaKhachHang")
 	private String maKhachHang;
+	
+	@Column(name = "HoTen")
     private String hoTen;
+	
+	@Column(name = "SoDienThoai")
     private String soDienThoai;
+	
+	@Column(name = "DiaChi")
     private String diaChi;
+	
+	@Column(name = "nhomKhachHang")
     private String nhomKhachHang;
     
 }
