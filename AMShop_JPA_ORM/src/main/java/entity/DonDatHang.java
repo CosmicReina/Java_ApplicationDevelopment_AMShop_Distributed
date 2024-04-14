f0a4b120a1b8cdcdc4d63d661ae8ff46770a9745
NguyenHuuPhuocHai <hain35284@gmail.com>
2024-04-14 22:07:08 +0700

    - Update DonDatHang

--- AMShop_JPA_ORM/src/main/java/entity/DonDatHang.java
+++ AMShop_JPA_ORM/src/main/java/entity/DonDatHang.java
@@ -3,6 +3,10 @@ package entity;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,14 +19,27 @@ import lombok.ToString;
 @EqualsAndHashCode
 @ToString
 
+@Entity
+@Table(name = "DonDatHang")
+
 public class DonDatHang implements Serializable {
 	
 	private static final long serialVersionUID = 4899708714148214926L;
 	
+	
+	@Column(name = "MaDonDatHang")
 	private String maDonDatHang;
+	
+	@Column(name = "NhanVien")
 	private NhanVien nhanVien;
+	
+	@Column(name = "KhachHang")
 	private KhachHang khachHang;
+	
+	@Column(name = "ThoiGianTao")
 	private LocalDateTime thoiGianTao;
+	
+	@Column(name = "TrangThaiThanhToan")
 	private boolean trangThaiThanhToan;
 	
 }
