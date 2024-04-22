06f505a5a48c9f1940c07c1843f68e295506ea14
NguyenHuuPhuocHai <hain35284@gmail.com>
2024-04-23 00:50:14 +0700

    Create JTC_NhaSanXuat.java

---
+++ AMShop_JPA_ORM_Client/src/test/java/_01_Client/JTC_NhaSanXuat.java
@@ -0,0 +1,51 @@
+package _01_Client;
+
+import java.rmi.RemoteException;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import configuration.ServiceInitiator;
+import service_interface.IService_NhaSanXuat;
+
+class JTC_NhaSanXuat {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void getAllNhaSanXuat() throws RemoteException {
+		System.err.println("getAllNhaSanXuat()");
+		
+		IService_NhaSanXuat service_NhaSanXuat = ServiceInitiator.getInstance().getServiceNhaSanXuat();
+		service_NhaSanXuat.getAllNhaSanXuat().forEach(System.out::println);
+		
+		System.out.println("\n");
+	}
+
+	@Test
+	void kiemTraTonTai() throws RemoteException {
+		System.err.println("kiemTraTonTai()");
+		
+		IService_NhaSanXuat service_NhaSanXuat = ServiceInitiator.getInstance().getServiceNhaSanXuat();
+		System.out.println(service_NhaSanXuat.kiemTraTonTai("Adidas"));
+		
+		System.out.println("\n");
+	}
+}
