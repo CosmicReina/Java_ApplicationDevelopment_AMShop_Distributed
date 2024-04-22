675ce0fb0ab7e38a028e2c3d72280a86ce99f45f
Rei <snowy.sunny.dazing.us@gmail.com>
2024-04-22 13:00:18 +0900

    - Update

--- ApplicationDevelopment_AMShop/src/initiate/Initiate.java
+++ ApplicationDevelopment_AMShop/src/initiate/Initiate.java
@@ -5,7 +5,7 @@ import gui.GUI_DangNhap;
 
 public class Initiate {
     public static void main(String[] args) {
-        ConnectDB.getInstance().connectDatabase();
+//        ConnectDB.getInstance().connectDatabase();
         GUI_DangNhap gui = GUI_DangNhap.getInstance();
         gui.setVisible(true);
     }
