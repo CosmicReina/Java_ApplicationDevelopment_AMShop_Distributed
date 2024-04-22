8dc588c6251b82308efd93b8aeffaec190f2ce72
NguyenHuuPhuocHai <hain35284@gmail.com>
2024-04-22 11:46:20 +0700

    Create IService_KhachHang.java

---
+++ AMShop_JPA_ORM_Server/src/main/java/service_interface/IService_KhachHang.java
@@ -0,0 +1,18 @@
+package service_interface;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+import entity.KhachHang;
+
+public interface IService_KhachHang extends Remote{
+
+	public List<KhachHang> getAllKhachHang() throws RemoteException;
+	
+	public KhachHang getKhachHangTheoMaKhachHang(String maKhachHang) throws RemoteException;
+	
+	public KhachHang getKhachHangTheoSoDienThoai(String soDienThoai) throws RemoteException;
+	
+	public KhachHang getKhachHangCuoi(String prefix) throws RemoteException;
+}
