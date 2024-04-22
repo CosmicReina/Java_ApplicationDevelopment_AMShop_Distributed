5ce4e59ba2c872224038da72f21e1dff8f765a42
Rei <snowy.sunny.dazing.us@gmail.com>
2024-04-22 13:52:14 +0900

    - Update

--- AMShop_JPA_ORM_Server/src/main/java/configuration/Configuration.java
+++ AMShop_JPA_ORM_Server/src/main/java/configuration/Configuration.java
@@ -3,18 +3,26 @@ package configuration;
 import entity.NhanVien;
 
 public class Configuration {
-	
+
+	private Configuration() {
+	}
+
 	private static final String HOST = "REI";
 	private static final int PORT = 8080;
 	private static final String URL = "rmi://" + HOST + ":" + PORT + "/";
+
+	public static String getURL() {
+		return URL;
+	}
 	
 	private static NhanVien nhanVienHienTai = null;
 	
-	private Configuration() {
+	public static NhanVien getNhanVienHienTai() {
+		return nhanVienHienTai;
 	}
 	
-	public static String getURL() {
-		return URL;
+	public static void setNhanVienHienTai(NhanVien nhanVienHienTai) {
+		Configuration.nhanVienHienTai = nhanVienHienTai;
 	}
 
 }
