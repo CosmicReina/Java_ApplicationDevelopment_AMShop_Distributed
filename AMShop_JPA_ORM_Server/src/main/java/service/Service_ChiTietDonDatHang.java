47e4d02ec9c2cd77b31b15ba9c080bfed69055d4
NguyenHuuPhuocHai <hain35284@gmail.com>
2024-04-22 12:16:43 +0700

    Create Service_ChiTietDonDatHang.java

---
+++ AMShop_JPA_ORM_Server/src/main/java/service/Service_ChiTietDonDatHang.java
@@ -0,0 +1,26 @@
+package service;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.List;
+
+import dao.DAO_ChiTietDonDatHang;
+import entity.ChiTietDonDatHang;
+import service_interface.IService_ChiTietDonDatHang;
+
+public class Service_ChiTietDonDatHang extends UnicastRemoteObject implements IService_ChiTietDonDatHang{
+
+	private static final long serialVersionUID = 4561975764196512284L;
+
+	protected Service_ChiTietDonDatHang() throws RemoteException {
+		super();
+	}
+
+	@Override
+	public List<ChiTietDonDatHang> getAllChiTietDonDatHangTheoMaDonDatHang(String maDonDatHang) throws RemoteException {
+		return DAO_ChiTietDonDatHang.getAllChiTietDonDatHangTheoMaDonDatHang(maDonDatHang);
+	}
+
+	
+	
+}
