5bf9983ddaf1f6fe8317a1676f34ce69c449b9c4
Rei <snowy.sunny.dazing.us@gmail.com>
2024-04-23 02:48:43 +0900

    - Update

--- AMShop_JPA_ORM_Server/src/main/java/dao/DAO_ChiTietDonDatHang.java
+++ AMShop_JPA_ORM_Server/src/main/java/dao/DAO_ChiTietDonDatHang.java
@@ -5,11 +5,12 @@ import java.util.List;
 import connection.ConnectionMSSQL;
 import entity.ChiTietDonDatHang;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 
 public class DAO_ChiTietDonDatHang {
-	
+
 	private static EntityManager entityManager = ConnectionMSSQL.getEntityManager();
-	
+
 	private DAO_ChiTietDonDatHang() {
 	}
 
@@ -18,26 +19,33 @@ public class DAO_ChiTietDonDatHang {
 			entityManager.getTransaction().begin();
 			entityManager.persist(chiTietDonDatHang);
 			entityManager.getTransaction().commit();
-		}catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			entityManager.getTransaction().rollback();
 		}
 	}
-	
+
 	public static void deleteChiTietDonDatHang(ChiTietDonDatHang chiTietDonDatHang) {
 		try {
 			entityManager.getTransaction().begin();
 			entityManager.remove(chiTietDonDatHang);
 			entityManager.getTransaction().commit();
-		}catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			entityManager.getTransaction().rollback();
 		}
 	}
-	
-	public static List<ChiTietDonDatHang> getAllChiTietDonDatHangTheoMaDonDatHang(String maDonDatHang){
-		return entityManager.createNamedQuery("ChiTietDonDatHang.getAllChiTietDonDatHangTheoMaDonDatHang", ChiTietDonDatHang.class)
+
+	public static List<ChiTietDonDatHang> getAllChiTietDonDatHangTheoMaDonDatHang(
+			String maDonDatHang) {
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+		List<ChiTietDonDatHang> list = entityManager
+				.createNamedQuery("ChiTietDonDatHang.getAllChiTietDonDatHangTheoMaDonDatHang",
+						ChiTietDonDatHang.class)
 				.setParameter("maDonDatHang", maDonDatHang)
 				.getResultList();
+		entityTransaction.commit();
+		return list;
 	}
 }
