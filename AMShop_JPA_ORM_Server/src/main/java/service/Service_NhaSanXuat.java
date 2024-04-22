3284a8f423f2f3eadb3f41a6cf5a50ab1efaad6f
NguyenHuuPhuocHai <hain35284@gmail.com>
2024-04-22 12:16:25 +0700

    Create Service_NhaSanXuat.java

---
+++ AMShop_JPA_ORM_Server/src/main/java/service/Service_NhaSanXuat.java
@@ -0,0 +1,31 @@
+package service;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.List;
+
+import dao.DAO_NhaSanXuat;
+import entity.NhaSanXuat;
+import service_interface.IService_NhaSanXuat;
+
+public class Service_NhaSanXuat extends UnicastRemoteObject implements IService_NhaSanXuat {
+
+	private static final long serialVersionUID = 4945790864115092327L;
+
+	protected Service_NhaSanXuat() throws RemoteException {
+		super();
+	}
+
+	@Override
+	public List<NhaSanXuat> getAllNhaSanXuat() throws RemoteException {
+		return DAO_NhaSanXuat.getAllNhaSanXuat();
+	}
+
+	@Override
+	public NhaSanXuat kiemTraTonTai(String tenNhaSanXuat) throws RemoteException {
+		return DAO_NhaSanXuat.kiemTraTonTai(tenNhaSanXuat);
+	}
+
+	
+	
+}
