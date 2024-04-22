4e321d4aec488cf8e6a75de5c9910551cef4fd15
Rei <snowy.sunny.dazing.us@gmail.com>
2024-04-22 16:49:43 +0900

    - Update

---
+++ AMShop_JPA_ORM_Server/src/main/java/data/KhoiTaoMa.java
@@ -0,0 +1,130 @@
+package data;
+
+import dao_old.DAO_DonDatHang;
+import dao_old.DAO_HoaDon;
+import dao_old.DAO_KhachHang;
+import dao_old.DAO_NhanVien;
+import dao_old.DAO_QuanAo;
+import java.time.LocalDate;
+
+public class KhoiTaoMa {
+    public static String generateMaNhanVien(){
+        String maNhanVien;
+        int soNhanVien;
+        
+        LocalDate localDate = LocalDate.now();
+        String year = Integer.toString(localDate.getYear()).substring(2);
+        String month = String.format("%02d", localDate.getMonth().getValue());
+        String day = String.format("%02d", localDate.getDayOfMonth());
+        
+        String prefix = "NV" + year + month + day;
+        
+        String maNhanVienCuoi = DAO_NhanVien.getMaNhanVienCuoi(prefix);
+        if(maNhanVienCuoi == null){
+            soNhanVien = 1;
+            maNhanVien = prefix + String.format("%02d", soNhanVien);
+        }
+        else{
+            soNhanVien = Integer.parseInt(maNhanVienCuoi.substring(8)) + 1;
+            maNhanVien = prefix + String.format("%02d", soNhanVien);
+        }
+        return maNhanVien;
+    }
+    
+    public static String generateMaKhachHang(){
+        String maKhachHang;
+        int soKhachHang;
+        
+        LocalDate localDate = LocalDate.now();
+        String year = Integer.toString(localDate.getYear()).substring(2);
+        
+        String prefix = "KH" + year;
+        
+        String maKhachHangCuoi = DAO_KhachHang.getMaKhachHangCuoi(prefix);
+        if(maKhachHangCuoi == null){
+            soKhachHang = 1;
+            maKhachHang = prefix + String.format("%06d", soKhachHang);
+        }
+        else{
+            soKhachHang = Integer.parseInt(maKhachHangCuoi.substring(4)) + 1;
+            maKhachHang = prefix + String.format("%06d", soKhachHang);
+        }
+        return maKhachHang;
+    }
+    
+    public static String generateMaQuanAo(){
+        String maQuanAo;
+        int soQuanAo;
+        
+        String prefix = "QA";
+        
+        String maQuanAoCuoi = DAO_QuanAo.getMaQuanAoCuoi();
+        if(maQuanAoCuoi == null){
+            soQuanAo = 1;
+            maQuanAo = prefix + String.format("%06d", soQuanAo);
+        }
+        else{
+            soQuanAo = Integer.parseInt(maQuanAoCuoi.substring(2)) + 1;
+            maQuanAo = prefix + String.format("%06d", soQuanAo);
+        }
+        return maQuanAo;
+    }
+    
+    public static String generateMaHoaDon(){
+        String maHoaDon;
+        int soHoaDon;
+        
+        LocalDate localDate = LocalDate.now();
+        String year = Integer.toString(localDate.getYear()).substring(2);
+        String month = String.format("%02d", localDate.getMonth().getValue());
+        String day = String.format("%02d", localDate.getDayOfMonth());
+        
+        String prefix = "HD" + year + month + day;
+        
+        String maHoaDonCuoi = DAO_HoaDon.getMaHoaDonCuoi(prefix);
+        if(maHoaDonCuoi == null){
+            soHoaDon = 1;
+            maHoaDon = prefix + String.format("%04d", soHoaDon);
+        }
+        else{
+            soHoaDon = Integer.parseInt(maHoaDonCuoi.substring(8)) + 1;
+            maHoaDon = prefix + String.format("%04d", soHoaDon);
+        }
+        return maHoaDon;
+    }
+    
+    public static String generateMaDonDatHang(){
+        String maDonDatHang;
+        int soDonDatHang;
+        
+        LocalDate localDate = LocalDate.now();
+        String year = Integer.toString(localDate.getYear()).substring(2);
+        String month = String.format("%02d", localDate.getMonth().getValue());
+        String day = String.format("%02d", localDate.getDayOfMonth());
+        
+        String prefix = "DD" + year + month + day;
+        
+        String maDonDatHangCuoi = DAO_DonDatHang.getMaDonDatHangCuoi(prefix);
+        if(maDonDatHangCuoi == null){
+            soDonDatHang = 1;
+            maDonDatHang = prefix + String.format("%04d", soDonDatHang);
+        }
+        else{
+            soDonDatHang = Integer.parseInt(maDonDatHangCuoi.substring(8)) + 1;
+            maDonDatHang = prefix + String.format("%04d", soDonDatHang);
+        }
+        return maDonDatHang;
+    }
+    
+    public static String generateMaLichLamViec(LocalDate localDate, String caLamViec){
+        String maLichLamViec;
+        
+        String year = Integer.toString(localDate.getYear()).substring(2);
+        String month = String.format("%02d", localDate.getMonth().getValue());
+        String day = String.format("%02d", localDate.getDayOfMonth());
+        
+        maLichLamViec = "LH" + year + month + day + caLamViec;
+        
+        return maLichLamViec;
+    }
+}
---
+++ AMShop_JPA_ORM_Server/src/main/java/gui_old/GUI_TinhLuongNhanVien.java
@@ -0,0 +1,191 @@
+package gui_old;
+
+import dao_old.DAO_NhanVien;
+import data.FormatDouble;
+import entity_old.NhanVien;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import java.sql.*;
+import java.time.Duration;
+
+public class GUI_TinhLuongNhanVien extends javax.swing.JPanel {
+    
+    private static GUI_TinhLuongNhanVien instance = new GUI_TinhLuongNhanVien();
+    
+    public static GUI_TinhLuongNhanVien getInstance() {
+        return instance;
+    }
+    
+    public static GUI_TinhLuongNhanVien newInstance() {
+        instance = new GUI_TinhLuongNhanVien();
+        return instance;
+    }
+    
+    public GUI_TinhLuongNhanVien() {
+        initComponents();
+        tblLuong.fixTable(scrLuong);
+        
+    }
+    
+    private void tinhLuongNhanVien(){
+        int nam;
+        int thang;
+        try{
+            nam = Integer.parseInt(txtNam.getText());
+            thang = Integer.parseInt(txtThang.getText());
+        } catch(NumberFormatException ex){
+            JOptionPane.showMessageDialog(null, "Vui lòng nhập Năm và Tháng hợp lệ");
+            return;
+        }
+        ResultSet rs = DAO_NhanVien.tinhLuongNhanVien(nam, thang);
+        try {
+            DefaultTableModel model = (DefaultTableModel) tblLuong.getModel();
+            model.getDataVector().removeAllElements();
+            tblLuong.revalidate();
+            tblLuong.repaint();
+            while(rs.next()){
+                String maNhanVien = rs.getString(1);
+                NhanVien nhanVien = DAO_NhanVien.getNhanVienTheoMaNhanVien(maNhanVien);
+                
+                int thoiGianLam = rs.getInt(2);
+                Duration duration = Duration.ofSeconds(thoiGianLam);
+                long gio = duration.toHours();
+                long phut = duration.toMinutesPart();
+                long giay = duration.toSecondsPart();
+                
+                double thoiGianLamViec = gio + phut/60 + giay/3600;
+                
+                model.addRow(new Object[]{
+                    maNhanVien,
+                    nhanVien.getHoTen(),
+                    nhanVien.getChucVu(),
+                    nhanVien.getLuong(),
+                    thoiGianLamViec,
+                    FormatDouble.toMoney(nhanVien.getLuong() * thoiGianLamViec)
+                });
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+        if(tblLuong.getModel().getRowCount() == 0)
+            JOptionPane.showMessageDialog(null, "Không có dữ liệu");
+    }
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        pnlHoaDon = new javax.swing.JPanel();
+        scrLuong = new javax.swing.JScrollPane();
+        tblLuong = new extended_component.JTable_LightMode();
+        pnlThongKe = new javax.swing.JPanel();
+        lblNam = new javax.swing.JLabel();
+        lblThang = new javax.swing.JLabel();
+        txtNam = new extended_component.JTextField_AllRound();
+        txtThang = new extended_component.JTextField_AllRound();
+        btnTinhLuong = new extended_component.JButton_AllRound();
+
+        setBackground(new java.awt.Color(68, 136, 255));
+        setLayout(new java.awt.BorderLayout());
+
+        pnlHoaDon.setOpaque(false);
+        pnlHoaDon.setPreferredSize(new java.awt.Dimension(600, 700));
+        pnlHoaDon.setLayout(new java.awt.BorderLayout());
+
+        scrLuong.setBackground(new java.awt.Color(68, 136, 255));
+        scrLuong.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Lương Nhân Viên", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.TOP, new java.awt.Font("Segoe UI", 0, 24), new java.awt.Color(255, 255, 255))); // NOI18N
+
+        tblLuong.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Mã Nhân Viên", "Tên Nhân Viên", "Chức Vụ", "Mức Lương", "Tổng Giờ Làm", "Lương Tháng"
+            }
+        ));
+        scrLuong.setViewportView(tblLuong);
+
+        pnlHoaDon.add(scrLuong, java.awt.BorderLayout.CENTER);
+
+        add(pnlHoaDon, java.awt.BorderLayout.CENTER);
+
+        pnlThongKe.setBackground(new java.awt.Color(68, 136, 255));
+        pnlThongKe.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Tính Lương Tháng", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.TOP, new java.awt.Font("Segoe UI", 0, 24), new java.awt.Color(255, 255, 255))); // NOI18N
+        pnlThongKe.setPreferredSize(new java.awt.Dimension(300, 100));
+
+        lblNam.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        lblNam.setForeground(new java.awt.Color(255, 255, 255));
+        lblNam.setText("Năm");
+
+        lblThang.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        lblThang.setForeground(new java.awt.Color(255, 255, 255));
+        lblThang.setText("Tháng");
+
+        txtNam.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+
+        txtThang.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+
+        btnTinhLuong.setText("Tính Lương");
+        btnTinhLuong.setBorderRadius(30);
+        btnTinhLuong.setColorBackground(new java.awt.Color(170, 238, 255));
+        btnTinhLuong.setColorBorder(new java.awt.Color(255, 255, 255));
+        btnTinhLuong.setColorClick(new java.awt.Color(119, 204, 255));
+        btnTinhLuong.setColorEnter(new java.awt.Color(119, 238, 255));
+        btnTinhLuong.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnTinhLuongActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout pnlThongKeLayout = new javax.swing.GroupLayout(pnlThongKe);
+        pnlThongKe.setLayout(pnlThongKeLayout);
+        pnlThongKeLayout.setHorizontalGroup(
+            pnlThongKeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnlThongKeLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(lblNam)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(txtNam, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(lblThang)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(txtThang, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 214, Short.MAX_VALUE)
+                .addComponent(btnTinhLuong, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        pnlThongKeLayout.setVerticalGroup(
+            pnlThongKeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnlThongKeLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(pnlThongKeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addComponent(btnTinhLuong, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(pnlThongKeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(txtNam, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(lblThang)
+                        .addComponent(lblNam)
+                        .addComponent(txtThang, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(14, Short.MAX_VALUE))
+        );
+
+        add(pnlThongKe, java.awt.BorderLayout.SOUTH);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnTinhLuongActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnTinhLuongActionPerformed
+        // TODO add your handling code here:
+        tinhLuongNhanVien();
+    }//GEN-LAST:event_btnTinhLuongActionPerformed
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private extended_component.JButton_AllRound btnTinhLuong;
+    private javax.swing.JLabel lblNam;
+    private javax.swing.JLabel lblThang;
+    private javax.swing.JPanel pnlHoaDon;
+    private javax.swing.JPanel pnlThongKe;
+    private javax.swing.JScrollPane scrLuong;
+    private extended_component.JTable_LightMode tblLuong;
+    private extended_component.JTextField_AllRound txtNam;
+    private extended_component.JTextField_AllRound txtThang;
+    // End of variables declaration//GEN-END:variables
+
+}
