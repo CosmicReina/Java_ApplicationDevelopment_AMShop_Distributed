2acde4b2d3ee3ab6b5016cef204d0d1d6411c479
Rei <snowy.sunny.dazing.us@gmail.com>
2024-04-25 15:15:07 +0900

    - Update

--- AMShop_JPA_ORM_Client/src/main/java/client/GUI_Initiate.java
+++ AMShop_JPA_ORM_Client/src/main/java/client/GUI_Initiate.java
@@ -6,16 +6,11 @@ import java.rmi.RemoteException;
 
 import configuration.Configuration_Client;
 import configuration.ServiceInitiator;
-import gui.GUI_DangNhap;
 import gui.GUI_MainFrame;
 
-@SuppressWarnings("unused")
 public class GUI_Initiate {
 	
-	public static void main(String[] args) throws RemoteException, MalformedURLException, NotBoundException {
-//		GUI_DangNhap gui_DangNhap = GUI_DangNhap.getInstance();
-//		gui_DangNhap.setVisible(true);
-		
+	public static void main(String[] args) throws RemoteException, MalformedURLException, NotBoundException {		
 		Configuration_Client.setNhanVienHienTai(ServiceInitiator.getInstance().getServiceNhanVien().getNhanVienTheoMaNhanVien("NV12312312"));
 		GUI_MainFrame gui_MainFrame = GUI_MainFrame.newInstance();
 		gui_MainFrame.setVisible(true);
